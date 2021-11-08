35d2ae5d7c5c71c014ae651a440311caefbeebcd
wampet <wampet232@gmail.com>
2021-11-08 08:56:19 +0300

    feat: added new pages and a custom card

--- lib/main.dart
+++ lib/main.dart
@@ -1,83 +1,18 @@
-// ignore_for_file: prefer_const_constructors
 
-import 'dart:async';
-
+import 'package:chapchap_kyc_frontend/pages/cards_page.dart';
+import 'package:chapchap_kyc_frontend/pages/final_page.dart';
+import 'package:chapchap_kyc_frontend/pages/selfie.dart';
+import 'package:chapchap_kyc_frontend/pages/splashscreen.dart';
 import 'package:flutter/material.dart';
 
 
-void main(){
-  runApp(Splash());
-}
+void main ()=> runApp(MaterialApp(
+initialRoute: '/selfieUpload',
+routes: {
+'/selfieUpload': (context)=> const SelfieUpload(),
+'/splashScreen':(context)=> const SplashScreen(),
+'/finalScreen': (context)=>const FinalScreen(),
+'/cardspage': (context)=>const CardsPage(),
 
-// ignore: use_key_in_widget_constructors
-class Splash extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      home: SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({ Key? key }) : super(key: key);
-
-  BuildContext? get context => null;
- @override
-// ignore: override_on_non_overriding_member
-void initState(){
-  //  super.initState();
-   Timer(Duration(seconds:3),(){
-     Navigator.of(context!).
-     pushReplacement(MaterialPageRoute(builder: (_)=>Homepage()));
-   });
- }
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset('assets/chapchap-logo.png',height:200,width: 200,),
-            // SizedBox(height: 10,),
-            Text('Grow Your Business',style: TextStyle(color: Colors.red,fontSize:25)),
-            SizedBox(height: 10,),
-            CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation<Color>(Colors.red),
-            )
-          ],
-        ),
-      ),
-      
-    );
-  }
-}
-
- class Homepage extends StatefulWidget {
-  const Homepage({ Key? key }) : super(key: key);
-
-  @override
-  _HomepageState createState() => _HomepageState();
-}
-
-class _HomepageState extends State<Homepage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor:Colors.red[200],
-        title: Text('Profiles'),
-      ),
-      body: Center(
-        child:Text('Home sweet home')
-      ),
-    );
-  }
-}
+},
+));
---
+++ lib/pages/business_location.dart
@@ -0,0 +1,138 @@
+import 'package:chapchap_kyc_frontend/kyc_icons_icons.dart';
+import 'package:flutter/material.dart';
+
+class SelfieUpload extends StatefulWidget {
+  const SelfieUpload({Key? key}) : super(key: key);
+
+  @override
+  State<SelfieUpload> createState() => _SelfieUploadState();
+}
+
+class _SelfieUploadState extends State<SelfieUpload> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Registerd Company',
+            style: TextStyle(
+              color: Colors.red,
+            )),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.arrow_back),
+          color: Colors.red,
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: const BoxConstraints(maxHeight: 600.0),
+          child: Column(
+            children: <Widget>[
+              Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(20, 20, 10, 0),
+                    child: const Text(
+                      'Progress',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(20, 3, 20, 0),
+                    padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                    child: const LinearProgressIndicator(
+                        value: 0.7,
+                        backgroundColor: Colors.grey,
+                        minHeight: 10,
+                        color: Colors.red),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(20, 40, 20, 50),
+                    child: const Text(
+                      'Upload your business Premises',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(30, 40, 20, 50),
+                    child: const Text(
+                      'pending',
+                      style: TextStyle(
+                        color: Colors.orange,
+                        fontSize: 9,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Icon(KycIcons.add_a_photo, size: 80, color: Colors.red),
+                radius: 80,
+              ),
+              Column(
+                children: [
+                  // ignore: avoid_unnecessary_containers
+                  Container(
+                    margin: const EdgeInsets.fromLTRB(20, 60, 20, 50),
+                    child: const Text(
+                      'Your upload will help us to know your location and to serve you better.',
+                      style: TextStyle(color: Colors.black, fontSize: 16),
+                    ),
+                  ),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: const Text(
+                          '1 of 5',
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        padding:const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                        child: TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(context, '/cardspage');
+                            },
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.red),
+                            ),
+                            child: const Text('Next',
+                                style: TextStyle(color: Colors.white))),
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
