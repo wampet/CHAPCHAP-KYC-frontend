bc869d582aa684353321c16764c4382ff27d7899
dorothyas <dorotheeasiimwe@gmail.com>
2021-11-17 15:12:15 +0300

    update progress bar

--- lib/pages/business_location.dart
+++ lib/pages/business_location.dart
@@ -1,5 +1,6 @@
 import 'package:chapchap_kyc_frontend/kyc_icons_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:step_progress_indicator/step_progress_indicator.dart';
 
 
 class BusinessLocation extends StatefulWidget {
@@ -38,23 +39,14 @@ class _BusinessLocationState extends State<BusinessLocation> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
-                    margin: const EdgeInsets.fromLTRB(20, 20, 10, 0),
-                    child: const Text(
-                      'Progress',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 14,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(20, 3, 20, 0),
+                    margin: const EdgeInsets.fromLTRB(20, 20, 20, 0),
                     padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    child: const LinearProgressIndicator(
-                        value: 0.7,
-                        backgroundColor: Colors.grey,
-                        minHeight: 10,
-                        color: Colors.red),
+                    child: const StepProgressIndicator(
+                                totalSteps: 5,
+                                currentStep: 5,
+                                selectedColor: Colors.red,
+                                unselectedColor: Colors.grey
+                            ),
                   ),
                 ],
               ),
